b637bbb38c661ded56e37842e11760bfda203ea3
James Leask <jl@occam.global>
2023-08-22 16:24:30 +0800

    adding node package and javascript

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get 'puppeteer/interact_with_chatlio'
+  # get 'puppeteer/interact_with_chatlio'
+  get 'puppeteer/type_message_in_text_field'
+
+  # JS route
+  get 'puppeteer/run_script'
 
 end
